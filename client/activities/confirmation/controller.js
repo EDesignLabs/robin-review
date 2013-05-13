bc3773123837d04bbf13b248e37d9b370d47d6a9
Luke BrownGold <lbrowngold@lsc.org>
2013-05-13 09:41:36 -0400

    confirmation activity

---
+++ client/activities/confirmation/controller.js
@@ -0,0 +1,25 @@
+Template.confirmationStructure.events = {
+	"click button": function (event){
+		Helpers.updateStructure({
+			highlight: $('input#highlight').val(),
+			question: $('input#question').val(),
+			noun: $('input#noun').val(),
+		})
+	}
+};
+
+Template.confirmationCreate.events = {
+	"click button": function (event){
+		Template.loop.create({
+			userHighlighted: $('#highlighter-confirm').text()
+		})
+	}
+};
+
+Template.confirmationAction.events = {
+	"click button#complete": function (event){
+		Template.loop.complete({
+			confirmation: $('#highlighter-confirm').text()
+		})
+	}
+};
